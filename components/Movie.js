85ef9723cae5eaef88d33db3d2026c4935d955ad
TomB-S <147733386+TomB-S@users.noreply.github.com>
2024-02-07 23:26:43 +0100

    i am updating the css for the website. Header is done, ongoing is card boxes

--- components/Movie.js
+++ components/Movie.js
@@ -48,8 +48,8 @@ function Movie(props) {
   return (
     <div className={styles.card}>
       <img className={styles.image} src={props.poster} alt={props.title} />
-      <div className={styles.textContainer}>
-        <div>
+      <div >
+        <div className={styles.textContainer}>
           <span className={styles.name}>{props.title}</span>
           <p className={styles.description}>{props.overview}</p>
         </div>
